59b1b04f2b16009b639262705f3aac8f7a7614dd
lnxlnxlnx <a16725798566@163.com>
2025-01-08 11:47:32 +0800

    first commit

--- main.cpp
+++ main.cpp
@@ -7,5 +7,19 @@ using namespace std;
 int main() {
 
     cout<<"hello_world"<<std::endl;
+    cout<<" ===============欢迎来到机房预约系统==============="<<std::endl;
+    cout<<""<<std::endl;
+    cout<<"请输入您的身份:"<<std::endl;
+    cout<<"--------------------------------------------------"<<std::endl;
+    cout<<""<<std::endl;
+    cout<<"                     1.学生代表                    "<<std::endl;
+    cout<<std::endl;
+    cout<<"                     2.老    师                    "<<std::endl;
+    cout<<std::endl;
+    cout<<"                     3.管 理 员                    "<<std::endl;
+    cout<<std::endl;
+    cout<<"                     0.退    出                    "<<std::endl;
+    cout<<std::endl;
+    cout<<"---------------------------------------------------"<<std::endl;
     return 0;
 }
